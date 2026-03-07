50a79d04f8ca540241c82f2412fdac92f5d5b8a2
Simon Legner <Simon.Legner@gmail.com>
2026-03-07 15:27:22 +0100

    useSorted from @vueuse

--- src/StationList.vue
+++ src/StationList.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import StationEntry from "./StationEntry.vue";
+import { useSorted } from "@vueuse/core";
 
 const props = defineProps<{
   features: Geo.Feature[];
 }>();
 
-const sortedFeatures = computed(() =>
-  [...props.features].sort(
-    (a, b) =>
-      a.properties["LWD-Region"].localeCompare(b.properties["LWD-Region"]) ||
-      b.geometry.coordinates[1]! - a.geometry.coordinates[1]!,
-  ),
+const sortedFeatures = useSorted(
+  props.features,
+  (a, b) =>
+    a.properties["LWD-Region"].localeCompare(b.properties["LWD-Region"]) ||
+    b.geometry.coordinates[1]! - a.geometry.coordinates[1]!,
 );
 </script>
 
