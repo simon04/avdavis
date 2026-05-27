00be6f146fa4b78379820155c61934ef5c32d1f8
Simon Legner <Simon.Legner@gmail.com>
2026-05-27 22:27:14 +0200

    Migrate to vite-plus from vite
    https://viteplus.dev/

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
   build: { sourcemap: true },
   plugins: [vue()],
 });
